91ce815ffd14c11ccca37414fd357745cfa1a166
david <davida.dc@gmail.com>
2016-08-01 09:00:27 +0200

    Update day 8 solution to regular expressions instead of eval

--- day08/solution.js
+++ day08/solution.js
@@ -1,39 +1,30 @@
 /* possible way to solve this:
 
-
+	eval() will solve part one, but only chumps use eval
+	use regular expressions !
 
 */
 
 function solve(inputString)  {
 
-	/*  regular expressions (doesn't work :( )
-	// keep a raw version of the string
-	var raw = inputString;
-	var clean = inputString;
-
-	raw = raw.replace(/(\n)+/g, "");
-	console.log("Raw string length: "+ raw.length);
-	console.log(raw);
-	// remove all double quote characters and new line combinations:
-	clean = clean.replace(/("\n")+/g, "");
-	// remove leading and trailing double quote
-	clean = clean.substring(1, clean.length-1);
-	// remove slash double quotes:
-	clean = clean.replace(/(\\{1})("{1})+/g, "Q");
-	// remove double slashes:
-	clean = clean.replace(/(\\)(\\{1})+/g, "S");
-	// remove hex
-	clean = clean.replace(/(\\x[0-9a-f]{2})+/g, "H");
-	*/
+	/*  regular expressions */
 
 	// part 1:
 	var raw = inputString.replace(/(\n)+/g, "");
 	var lines = inputString.split("\n");
-	var clean1 = "";
 
-	for(var i=0; i<lines.length; i++) {
-		clean1 += eval(lines[i]);
-	}
+	var clean1 = inputString;
+	// remove all double quote characters and new line combinations:
+	clean1 = clean1.replace(/("\n")+/g, "");
+	// remove leading and trailing double quote
+	clean1 = clean1.substring(1, clean1.length-1);
+	// remove slash double quotes:
+	clean1 = clean1.replace(/(\\")/g, "Q");
+	// remove double slashes:
+	clean1 = clean1.replace(/(\\\\)/g, "S");
+	// remove hex
+	clean1 = clean1.replace(/(\\x[0-9a-f]{2})/g, "H");
+
 
 	// part 2:
 	var clean2 = "";
@@ -42,7 +33,7 @@ function solve(inputString)  {
 		cleaned = lines[i].replace(/(\\)/g, "\\\\");
 		clean2 += cleaned.replace(/(")/g, "\\\"");
 	}
-	console.log(clean2);
+	//console.log(clean2);
 
 
 	document.getElementById("aoc-output-container").innerHTML = "<p>";
